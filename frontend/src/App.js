c107cc7f5ae48114f37e08fbc7c6e3039c543cb4
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-10-30 00:45:08 -0600

    frontend scaffolding done

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import {} from 'components';
+import {} from 'views';
+
+const App = () => {
+  return <Router></Router>;
+};
+
+export default App;
